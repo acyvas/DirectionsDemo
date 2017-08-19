69889f503f1a7f3ce308832e86e0ccc69a3ac408
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2017-08-19 07:36:28 +0200

    pull

--- initialize.ps1
+++ initialize.ps1
@@ -27,9 +27,17 @@ Log("ScriptPath: $scriptPath")
 
 $baseUrl = $scriptPath.SubString(0,$scriptPath.LastIndexOf('/')+1)
 $setupScript = "c:\demo\setup.ps1"
-DownloadFile("${baseUrl}setup.ps1", $setupScript)
+DownloadFile -SourceUrl "${baseUrl}setup.ps1" -destinationFile $setupScript
 
+$imageName = "navdocker.azurecr.io/dynamics-nav:devpreview"
+Log "pull $imageName"
+docker login navdocker.azurecr.io -u 7cc3c660-fc3d-41c6-b7dd-dd260148fff7 -p G/7gwmfohn5bacdf4ooPUjpDOwHIxXspLIFrUsGN+sU=
+docker pull $imageName
+
+Log "Register Setup Task"
 $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoExit $setupScript"
 $trigger = New-ScheduledTaskTrigger -AtLogOn
 Register-ScheduledTask -TaskName "setupScript" -Action $action -Trigger $trigger -RunLevel Highest -User $adminUser | Out-Null
+
+Log "Reboot and run Setup Task"
 Restart-Computer -Force
--- setup.ps1
+++ setup.ps1
@@ -3,8 +3,8 @@ schtasks /DELETE /TN setupScript /F
 
 function Log([string]$line, [string]$color = "Gray") { ("<font color=""$color"">" + [DateTime]::Now.ToString([System.Globalization.DateTimeFormatInfo]::CurrentInfo.ShortTimePattern.replace(":mm",":mm:ss")) + " $line</font>") | Add-Content -Path "c:\demo\status.txt" }
 
-$imageName = "navdocker.azurecr.io/dynamics-nav:devpreview"
+#$imageName = "navdocker.azurecr.io/dynamics-nav:devpreview"
 
-docker login navdocker.azurecr.io -u 7cc3c660-fc3d-41c6-b7dd-dd260148fff7 -p G/7gwmfohn5bacdf4ooPUjpDOwHIxXspLIFrUsGN+sU=
-docker pull $imageName
+#docker login navdocker.azurecr.io -u 7cc3c660-fc3d-41c6-b7dd-dd260148fff7 -p G/7gwmfohn5bacdf4ooPUjpDOwHIxXspLIFrUsGN+sU=
+#docker pull $imageName
 
