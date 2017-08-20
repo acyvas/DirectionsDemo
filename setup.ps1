97cf2858c2d185fa64db11a4b88108697bc7e946
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2017-08-20 23:34:46 +0200

    Blog post

--- setup.ps1
+++ setup.ps1
@@ -30,7 +30,9 @@ function New-DesktopShortcut([string]$Name, [string]$TargetPath, [string]$Workin
     }
 }
 
-Log "Install Docker PowerShell"
+Log -color Green "Finalizing Setup"
+
+Log "Installing Docker PowerShell"
 if (!(Get-PSRepository -Name DockerPS-Dev -ErrorAction Ignore)) {
     Register-PSRepository -Name DockerPS-Dev -SourceLocation https://ci.appveyor.com/nuget/docker-powershell-dev
 }
@@ -137,7 +139,7 @@ DownloadFile -SourceUrl "http://navserver:8080/$vsixName" -destinationFile $Vsix
 
 Log "install vsix"
 $code = "C:\Program Files (x86)\Microsoft VS Code\bin\Code.cmd"
-& $code @('--install-extension', $VsixFileName) | Log
+& $code @('--install-extension', $VsixFileName) | Out-Null
 
 $username = [Environment]::UserName
 if (Test-Path -path "c:\Users\Default\.vscode" -PathType Container -ErrorAction Ignore) {
@@ -146,20 +148,36 @@ if (Test-Path -path "c:\Users\Default\.vscode" -PathType Container -ErrorAction 
     }
 }
 
-Remove-Item "C:\DOWNLOAD\AL-master" -Recurse -Force -ErrorAction Ignore
-Remove-Item "C:\DOWNLOAD\VSCode" -Recurse -Force -ErrorAction Ignore
-Remove-Item "C:\DOWNLOAD\samples.zip" -Force -ErrorAction Ignore
-
+Log "Creating Desktop Shortcuts"
 New-DesktopShortcut -Name "Landing Page"                 -TargetPath "http://${hostname}:8080"                             -IconLocation "C:\Program Files\Internet Explorer\iexplore.exe, 3"
 New-DesktopShortcut -Name "Visual Studio Code"           -TargetPath "C:\Program Files (x86)\Microsoft VS Code\Code.exe"
 New-DesktopShortcut -Name "Web Client"                   -TargetPath "https://${hostname}/NAV/"                            -IconLocation "C:\Program Files\Internet Explorer\iexplore.exe, 3"
 New-DesktopShortcut -Name "Container Command Prompt"     -TargetPath "CMD.EXE"                                             -IconLocation "C:\Program Files\Docker\docker.exe, 0" -Arguments "/C docker.exe exec -it $containerID cmd"
 New-DesktopShortcut -Name "Container PowerShell Prompt"  -TargetPath "CMD.EXE"                                             -IconLocation "C:\Program Files\Docker\docker.exe, 0" -Arguments "/C docker.exe exec -it $containerID powershell"
 
+Log "Cleanup"
+Remove-Item "C:\DOWNLOAD\AL-master" -Recurse -Force -ErrorAction Ignore
+Remove-Item "C:\DOWNLOAD\VSCode" -Recurse -Force -ErrorAction Ignore
+Remove-Item "C:\DOWNLOAD\samples.zip" -Force -ErrorAction Ignore
+
+# Turn off IE Enhanced Security Configuration
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}" -Name "IsInstalled" -Value 0 | Out-Null
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}" -Name "IsInstalled" -Value 0 | Out-Null
+
+# Enable File Download in IE
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3" -Name "1803" -Value 0
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3" -Name "1803" -Value 0
+
+# Enable Font Download in IE
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3" -Name "1604" -Value 0
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3" -Name "1604" -Value 0
+
 # Remove Scheduled Task
 if (Get-ScheduledTask -TaskName setupScript -ErrorAction Ignore) {
     Log "Remove Scheduled Task"
-    schtasks /DELETE /TN setupScript /F
+    schtasks /DELETE /TN setupScript /F | Out-Null
 }
 
-Log "Installation Successfully completed"
+Start-Process "http://aka.ms/moderndevtools"
+
+Log -color Green "Setup Successfully completed"
